48360266fbac5fa1ec6b6e99d2dc7e132c6667f7
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-04-23 18:13:04 +0200

    JSnack 5 - Version 1.0

---
+++ jsnack5/main.js
@@ -0,0 +1,16 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
+
+//Chiedo all'utente quanti numeri vuole visualizzare come cubo e a partire da che numero
+var quantita = parseInt(prompt("Di quanti numeri vuoi calcolare il cubo?"));
+var num_partenza= parseInt(prompt("Da quale numero vuoi cominciare il conteggio?"));
+
+//Stampo a video i dati riguardanti le richieste dell'dati_utente
+document.getElementById("dati_utente").insertAdjacentHTML("beforeEnd","<p>Hai chiesto di calcolare il cubo " + quantita + " numeri</p>");
+document.getElementById("dati_utente").insertAdjacentHTML("beforeEnd","<p>Il calcolo comincia dal numero " + num_partenza + "</p>");
+
+//Calcolo e stampo il numero
+for (i = num_partenza; i < (num_partenza + quantita); i++) {
+    document.getElementById("base").insertAdjacentHTML("beforeEnd","<li>" + i + "</li>");
+    document.getElementById("potenza").insertAdjacentHTML("beforeEnd","<li>" + (i * i * i) + "</li>");
+    console.log(i * i * i);
+}
